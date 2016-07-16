f8854026523bdf42d15a640e0214f226bab8a1ea
somaakihiro <aki.air@icloud.com>
2016-07-16 11:23:35 +0900

    Messageにto_user_idカラム追加

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160715075216) do
+ActiveRecord::Schema.define(version: 20160716022029) do
 
   create_table "friendships", force: :cascade do |t|
     t.integer  "from_user_id"
@@ -25,6 +25,7 @@ ActiveRecord::Schema.define(version: 20160715075216) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer  "user_id"
+    t.integer  "to_user_id"
   end
 
   add_index "messages", ["user_id"], name: "index_messages_on_user_id"
