edc94f6925f6110b974031700537de4d94cc1b3a
somaakihiro <aki.air@icloud.com>
2016-08-29 13:50:40 +0900

    binding.pry削除

--- app/models/message.rb
+++ app/models/message.rb
@@ -6,7 +6,6 @@ class Message < ActiveRecord::Base
   def set_image(file)
     return if file.nil?
     file_name = Time.zone.now.to_i.to_s + file.original_filename
-    binding.pry
     File.open("public/message_images/#{file_name}", "wb") {|f|f.write(file.read)}
     self.image = file_name
   end
